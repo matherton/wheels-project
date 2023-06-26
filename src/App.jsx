cbdf4e23f9a46ccaafc14d67ca9f9bc026d1df6e
Mark Atherton <mark@insightinternet.co.uk>
2023-06-26 14:04:39 +0100

    added EWP logo and cleaned up boiler plate vue code from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+// import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import ewpLogo from "./assets/EWP-400x400.jpeg";
 import "./App.css";
 
 function App() {
@@ -9,11 +10,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://twitter.com/EdinburghWheels" target="_blank">
+          <img src={ewpLogo} className="logo" alt="Edinburgh Wheels Project" />
         </a>
       </div>
       <h1>Edinburgh Wheels Project</h1>
@@ -21,13 +19,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
