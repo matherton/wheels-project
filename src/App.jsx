196b5f9f9e24860f2d58496e43d9f41cb1bdbcef
Mark Atherton <mark@insightinternet.co.uk>
2023-06-28 14:37:10 +0100

    redesigned App with new graphics and wheel animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import ewpLogo from "./assets/ewp-cutout.jpeg";
+import wheel from "./assets/blank-wheel.png";
 import "./App.css";
 
 function App() {
@@ -9,12 +10,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://twitter.com/EdinburghWheels" target="_blank">
-          <img src={ewpLogo} className="logo" alt="Edinburgh Wheels Project" />
-        </a>
-      </div>
+      <a href="https://twitter.com/EdinburghWheels" target="_blank">
+        <img src={ewpLogo} alt="Edinburgh Wheels Project" />
+      </a>
       <h1>Edinburgh Wheels Project</h1>
+
+      <div style={{ borderTop: "0.5rem solid blanchedalmond" }}>
+        <img src={wheel} className="wheel" alt="" />
+        <img src={wheel} className="wheel" alt="" />
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
